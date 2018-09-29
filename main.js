7bc41bc58bf2cf68a121aa7773ff9f5b6a89267b
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-29 21:33:40 +0300

    Fixed a typo in perform method call

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ module.exports.loop = function () {
   for (var name in Game.creeps) {
     var creep = Game.creeps[name];
     var strategy = strategy_factory.get_strategy(creep);
-    strategy.peform(creep);
+    strategy.perform(creep);
   }
 }
