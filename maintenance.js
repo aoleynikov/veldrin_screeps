4a73bbe6006c60004c62fcef80376975b4c6fc46
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-29 22:51:33 +0300

    Use creep’s room in maintenance strategy

--- maintenance.js
+++ maintenance.js
@@ -1,6 +1,6 @@
 module.exports = {
   perform: function (creep) {
     creep.memory['role'] = 'maintenance';
-    creep.moveTo(Room.spawns['Main']);
+    creep.moveTo(creep.room.spawns['Main']);
   }
 }
