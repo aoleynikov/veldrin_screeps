46f7f8e9e891d55f25597acb43bd8e1a2c3d7f7d
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-29 21:26:21 +0300

    Fix strategy#perform call

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ module.exports.loop = function () {
   for (var i = 0; i < Game.creeps.length; ++i) {
     var creep = Game.creeps[i];
     var strategy = strategy_factory.get_strategy(creep);
-    strategy.peform()
+    strategy.peform(creep);
   }
 }
