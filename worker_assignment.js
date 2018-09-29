31cb34c617b5e3ffa9829f9dd87e351644805962
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-29 21:43:12 +0300

    Rearrange worker assignment module and add logging to it

--- worker_assignment.js
+++ worker_assignment.js
@@ -1,28 +1,30 @@
+var harvesting_positions = [];
+var harvesters = [];
+var dx = [0, 1, 1, -1, -1, -1, 0, 1];
+var dy = [1, 1, 0, 1, 0, -1, -1, -1];
+
 module.exports = {
-  harvesting_positions: [],
-  harvesters: [],
-  dx: [0, 1, 1, -1, -1, -1, 0, 1],
-  dy: [1, 1, 0, 1, 0, -1, -1, -1],
   init: function (room) {
     energy_sources = room.find(FIND_SOURCES);
     for (var source_index = 0; source_index < energy_sources.length; ++source_index) {
       source = energy_sources[source_index]
-      for (var i = 0; i < this.dx.length; ++i) {
+      for (var i = 0; i < dx.length; ++i) {
         new_position = {
-          x: source.pos.x + this.dx[i],
-          y: source.pos.y + this.dy[i]
+          x: source.pos.x + dx[i],
+          y: source.pos.y + dy[i]
         }
 
         terrain = room.getTerrain().get(new_position.x, new_position.y)
         if (terrain == 0) { // https://docs.screeps.com/api/#Room.Terrain.get
-          this.harvesting_positions.push(new_position);
+          harvesting_positions.push(new_position);
         }
       }
     }
+    console.log(harvesting_positions);
   },
   assign: function (creep) {
-    for (var i = 0; i < this.harvesting_positions.length; ++i) {
-      if (this.harvesters[i] === null) {
+    for (var i = 0; i < harvesting_positions.length; ++i) {
+      if (harvesters[i] === null) {
         harvesters[i] = creep;
         return;
       }
@@ -30,16 +32,16 @@ module.exports = {
   },
   get_workers_deficit: function () {
     var result = 0;
-    for (var i = 0; i < this.harvesting_positions.length; ++i) {
-      if (this.harvesters[i] === null) ++result;
+    for (var i = 0; i < harvesting_positions.length; ++i) {
+      if (harvesters[i] === null) ++result;
     }
     return result;
   },
   get_work_place: function (creep) {
-    for (var i = 0; i < this.harvesters.length; ++i) {
-      if (this.harvesters[i].id == creep.id) {
-        return this.hervesting_positions[i];
+    for (var i = 0; i < harvesters.length; ++i) {
+      if (harvesters[i].id == creep.id) {
+        return hervesting_positions[i];
       }
     }
   }
-};
+}
