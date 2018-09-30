517a7d35890eebc06499c2baeee92cae97a96562
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-30 20:23:59 +0300

    Added roads to repairable structures

--- room_wrapper.js
+++ room_wrapper.js
@@ -1,6 +1,7 @@
-var spawns = require('structure.spawn')
-var extensions = require('structure.extension')
-var containers = require('structure.container')
+var spawns = require('structure.spawn');
+var extensions = require('structure.extension');
+var containers = require('structure.container');
+var roads = require('structure.road');
 
 module.exports = {
     providers: [spawns, extensions, containers],
@@ -17,6 +18,11 @@ module.exports = {
     get_repairable_structures: function (room) {
         var result = room.find(FIND_MY_STRUCTURES);
         var added = containers.get(room);
+        for (var i = 0; i < added.length; ++i) {
+            result.push(added[i]);
+        }
+
+        added = roads.get(room)
         for (var i = 0; i < added.length; ++i) {
             result.push(added[i]);
         }
