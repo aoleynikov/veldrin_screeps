4ee80c2cdb5e2a31c0d55cd83ee8bee3a7297978
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-30 15:09:04 +0300

    Use parametrized if in room wrapper

--- room_wrapper.js
+++ room_wrapper.js
@@ -6,8 +6,8 @@ module.exports = {
     providers: [spawns, extensions, storages],
     get_energy_storages: function (room) {
         var result = []
-        for (var provider in this.providers) {
-            var structs = provider.get(room);
+        for (var i = 0; i < this.providers; ++i) {
+            var structs = providers[i].get(room);
             result = result.concat(structs);
         }
         return result;
