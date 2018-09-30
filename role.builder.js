8fc54f8037ce67dd12ef0137185331c05a8f4ffc
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-30 10:28:40 +0300

    Implement build and repair for builder role

--- role.builder.js
+++ role.builder.js
@@ -1,13 +1,28 @@
 var strategy = {
     build: function (creep) {
-        creep.room.find()
+        var construction_sites = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
+        for (var site in construction_sites) {
+            var work = creep.build(site);
+            if (work == ERR_NOT_IN_RANGE) {
+                creep.moveTo(site.pos.x, site.pos.y);
+            }
+            return true;
+        }
     },
     repair: function (creep) {
-
+        var structures = creep.room.find(FIND_MY_STRUCTURES);
+        for (var structure in structures) {
+            if (structure.hits < structure.hitsMax) {
+                var work = creep.repair(structure);
+                if (work == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(structure.pos.x, structure.pos.y);
+                }
+            }
+        }
     },
     upgrade: function (creep) {
-        controller = creep.room.controller;
-        work = creep.upgradeController(controller)
+        var controller = creep.room.controller;
+        var work = creep.upgradeController(controller)
         if (work != 0) {
             creep.moveTo(controller.pos.x, controller.pos.y);
         }
@@ -20,6 +35,8 @@ module.exports = {
             creep.memory['role'] = 'harvester';
             return;
         }
+        if (strategy.build(creep)) return;
+        if (strategy.repait(creep)) return;
         strategy.upgrade(creep);
     }
 }
