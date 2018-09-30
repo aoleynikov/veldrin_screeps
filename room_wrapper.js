283eac3c11f84eba26a2996409641d3ddbf68088
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-30 15:34:23 +0300

    Fixed another providers reference

--- room_wrapper.js
+++ room_wrapper.js
@@ -7,7 +7,7 @@ module.exports = {
     get_energy_storages: function (room) {
         var result = []
         for (var i = 0; i < this.providers.length; ++i) {
-            var structs = providers[i].get(room);
+            var structs = this.providers[i].get(room);
             for (var j = 0; j < structs.length; ++j) {
                 result.push(structs[j]);
             }
