c4707c1c54ec5fbe4f5cfbc0d23cd61970408b84
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-30 15:33:44 +0300

    Added lost .length

--- room_wrapper.js
+++ room_wrapper.js
@@ -6,7 +6,7 @@ module.exports = {
     providers: [spawns, extensions, storages],
     get_energy_storages: function (room) {
         var result = []
-        for (var i = 0; i < this.providers; ++i) {
+        for (var i = 0; i < this.providers.length; ++i) {
             var structs = providers[i].get(room);
             for (var j = 0; j < structs.length; ++j) {
                 result.push(structs[j]);
