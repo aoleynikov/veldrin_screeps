e9dc2c3dc7b76116ca194e1224663a996f3f0db0
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-30 11:10:19 +0300

    Use parametrized for in build as well

--- role.builder.js
+++ role.builder.js
@@ -1,10 +1,9 @@
 var strategy = {
     build: function (creep) {
         var construction_sites = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
-        for (var site in construction_sites) {
-            console.log(site);
+        for (var i = 0; i < construction_sites.length; ++i) {
+            site = construction_sites[i]
             var work = creep.build(site);
-            console.log(work);
             if (work == ERR_NOT_IN_RANGE) {
                 creep.moveTo(site.pos.x, site.pos.y);
             }
@@ -20,6 +19,7 @@ var strategy = {
                     creep.moveTo(structure.pos.x, structure.pos.y);
                 }
             }
+            return true;
         }
     },
     upgrade: function (creep) {
