0dd8e9a1a833101ea6d8996f7cf8329fbadb7097
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-09-30 16:22:54 +0300

    Adjust logging for storage selection

--- role.harvester.js
+++ role.harvester.js
@@ -28,6 +28,7 @@ var strategy = {
           return storages[i];
         }
       } else {
+        console.log(storages[i].keys())
         console.log(storages[i].store)
         if (storages[i].store < storages[i].storeCapacity) {
           return storages[i];
@@ -37,7 +38,6 @@ var strategy = {
   },
   store: function (creep) {
     var storage = this.select_storage(creep.room);
-    console.log(storage);
     // if (storage === undefined) {
     //   creep.memory['role'] = 'builder';
     // }
