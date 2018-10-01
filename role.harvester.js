47a662790e51c0ec0356d51bc14d1804685e57e3
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-10-01 16:21:35 +0300

    Also remove reference to worker assignment module

--- role.harvester.js
+++ role.harvester.js
@@ -3,31 +3,16 @@ var room_wrapper = require('room_wrapper')
 var strategy = {
   harvest: function (creep) {
     var provider = room_wrapper.get_energy_provider(creep.room);
+
     if (provider !== undefined) {
       harvest_from_storage = creep.withdraw(provider, RESOURCE_ENERGY);
       console.log(harvest_from_storage);
       if (harvest_from_storage == ERR_NOT_IN_RANGE) {
         creep.moveTo(provider);
-        return;
       } else if (harvest_from_storage == ERR_INVALID_ARGS) {
         creep.harvest(provider);
       }
     }
-
-    var work_place = creep.memory['work_place']
-    if (work_place === undefined) { // creep is not assigned
-      work_place = worker_assignment.assign(creep);
-    }
-    var at_work_place = work_place.x == creep.pos.x &&
-      work_place.y == creep.pos.y;
-
-    if (!at_work_place) {
-      creep.moveTo(work_place.x, work_place.y);
-      return;
-    }
-
-    var source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
-    creep.harvest(source);
   },
   select_storage: function (room) {
     var storages = room_wrapper.get_energy_storages(room);
