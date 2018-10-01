d61a4155b93809bd69a69345674294ec8f5f99af
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-10-01 17:13:46 +0300

    Introduce refill mechanics into upgrader as well

--- role.upgrader.js
+++ role.upgrader.js
@@ -5,12 +5,14 @@ var upgrade = function (creep) {
     var work = creep.upgradeController(controller)
     if (work == ERR_NOT_IN_RANGE) {
         creep.moveTo(controller.pos.x, controller.pos.y);
+    } else if (work == ERR_NOT_ENOUGH_ENERGY) {
+        creep.memory['refill'] = true;
     }
 }
 
 module.exports = {
     perform: function (creep) {
-        if (creep.carry[RESOURCE_ENERGY] == 0) {
+        if (creep.memory['refill']) {
             get_energy_behavior.perform(creep);
             return;
         }
