57d585f46d7ec01df8ca3a142e8a11b25a426e87
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-10-02 12:19:42 +0300

    Add factories to spawn manager

--- src/spawn_manager.js
+++ src/spawn_manager.js
@@ -1,10 +1,17 @@
 var harvester_factory = require('factory.harvester');
 var miner_factory = require('factory.miner');
+var builder_factory = require('factory.builder');
+var upgrader_factory = require('factory.upgrader');
+var repairer_factory = require('factory.repairer');
+
 var room_wrapper = require('room_wrapper');
 
 var factories = {
     harvester: harvester_factory,
-    miner: miner_factory
+    miner: miner_factory,
+    builder: builder_factory,
+    upgrader: upgrader_factory,
+    repairer: repairer_factory
 };
 
 var manager = {
