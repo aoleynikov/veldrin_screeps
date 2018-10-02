38005aae87df6f654de3ab0661624cf7576807af
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-10-02 23:13:34 +0300

    Implement own factory for harvesters

--- build/factory.harvester.js
+++ build/factory.harvester.js
@@ -1,1 +1,13 @@
-module.exports = require('factory.builder');
+module.exports = {
+  bodyparts: function (maxEnergy) {
+    maxEnergy -= 300;
+    var result = [MOVE, MOVE, WORK, WORK];
+
+    while (maxEnergy >= 50) {
+      maxEnergy -= 50;
+      result.push(CARRY);
+    }
+
+    return result;
+  }
+};
