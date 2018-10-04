a2b0323cf0e4f1c0add25590fc9ea5df585c96c5
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-10-04 11:44:19 +0300

    Initialize result in warrior factory

--- build/factory.warrior.js
+++ build/factory.warrior.js
@@ -1,5 +1,7 @@
 module.exports = {
   bodyparts: function (maxEnergy) {
+    var result = [];
+
     while (maxEnergy >= 130) {
       maxEnergy -= 130;
       result.push(ATTACK);
--- src/factories/factory.warrior.js
+++ src/factories/factory.warrior.js
@@ -1,5 +1,6 @@
 module.exports = {
     bodyparts: function (maxEnergy) {
+        var result = [];
         while (maxEnergy >= 130) {
             maxEnergy -= 130;
             result.push(ATTACK);
