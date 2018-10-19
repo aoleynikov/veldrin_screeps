871816bc0ca06939b8b22801875c91ed7503d32d
Andrey Oleynikov <aoleynikov@ruswizards.com>
2018-10-19 12:31:47 +0300

    Build recent changes

--- build/buildings_manager.js
+++ build/buildings_manager.js
@@ -1,10 +1,11 @@
-var tower_structure = require('structure.tower')
+var tower_structure = require('structure.tower');
 
 module.exports = {
-    run: function (room) {
-        var towers = tower_structure.get(room);
-        for (var i = 0; i < towers.length; ++i) {
-            tower_structure.shoot_on_sight(towers[i]);
-        }
+  run: function (room) {
+    var towers = tower_structure.get(room);
+
+    for (var i = 0; i < towers.length; ++i) {
+      tower_structure.shoot_on_sight(towers[i]);
     }
-}
+  }
+};
