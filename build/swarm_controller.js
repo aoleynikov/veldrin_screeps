355b2ccaa139efac59b0fa0c393c2cfb05d5fe99
Andrey Oleynikov <my_olein@mail.ru>
2018-10-19 22:20:26 +0300

    Move populartiopn to a separate module, add logging of swarm being complete

--- build/swarm_controller.js
+++ build/swarm_controller.js
@@ -6,129 +6,7 @@ income available to harvesters, the swarm doesn't function as intended.
 If you have a storage, it provides an easy way to monitor the economy balance.
 For RCL <= 3, don't get greedy.
 */
-var population = {
-  'W31S51': [{
-      count: 2,
-      role: 'upgrader',
-      body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
-      type: 'swarm'
-    },
-    {
-      count: 2,
-      role: 'repairer',
-      body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
-      type: 'swarm'
-    },
-    {
-      count: 3,
-      role: 'miner',
-      body: [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 1,
-      role: 'builder',
-      body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 3,
-      role: 'hauler',
-      body: [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY],
-      type: 'swarm',
-      energy_room: 'W32S51'
-    },
-    {
-      count: 3,
-      role: 'hauler',
-      body: [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY],
-      type: 'swarm',
-      energy_room: 'W31S52'
-    },
-    {
-      count: 4,
-      role: 'hauler',
-      body: [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY],
-      type: 'swarm',
-      energy_room: 'W32S52'
-    }
-  ],
-  'W32S51': [{
-      count: 1,
-      role: 'claimer',
-      body: [CLAIM, CLAIM, MOVE, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 2,
-      role: 'builder',
-      body: [WORK, MOVE, CARRY, MOVE, WORK, CARRY, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 2,
-      role: 'repairer',
-      body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
-      type: 'swarm'
-    },
-    {
-      count: 3,
-      role: 'miner',
-      body: [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE],
-      type: 'swarm'
-    }
-  ],
-  'W31S52': [{
-      count: 1,
-      role: 'claimer',
-      body: [CLAIM, CLAIM, MOVE, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 2,
-      role: 'builder',
-      body: [WORK, MOVE, CARRY, WORK, MOVE, CARRY, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 2,
-      role: 'repairer',
-      body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
-      type: 'swarm'
-    },
-    {
-      count: 3,
-      role: 'miner',
-      body: [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE],
-      type: 'swarm'
-    }
-  ],
-  'W32S52': [{
-      count: 1,
-      role: 'claimer',
-      body: [CLAIM, CLAIM, MOVE, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 2,
-      role: 'builder',
-      body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 3,
-      role: 'miner',
-      body: [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE],
-      type: 'swarm'
-    },
-    {
-      count: 2,
-      role: 'repairer',
-      body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
-      type: 'swarm'
-    }
-  ]
-};
+var population = require('population');
 
 var controller = {
   count_creeps: function (template, room_name) {
@@ -166,6 +44,9 @@ var controller = {
           }
         });
         console.log('[SWARM] spawning', template.role, ' for ', room_name, ': result - ', spawnResult);
+        if (spawnResult == 0) {
+          return true;
+        }
       }
     }
   }
@@ -180,9 +61,10 @@ module.exports = {
       for (var template of population[room_name]) {
         var actual = controller.count_creeps(template, room_name);
         if (actual < template.count) {
-          controller.spawnCreep(room_name, template)
+          if (controller.spawnCreep(room_name, template)) return;
         }
       }
     }
+    console.log('[SWARM] The swarm is fully developed.')
   }
 }
---
+++ src/population.js
@@ -0,0 +1,123 @@
+module.exports = {
+    'W31S51': [{
+            count: 2,
+            role: 'upgrader',
+            body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
+            type: 'swarm'
+        },
+        {
+            count: 2,
+            role: 'repairer',
+            body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
+            type: 'swarm'
+        },
+        {
+            count: 3,
+            role: 'miner',
+            body: [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 1,
+            role: 'builder',
+            body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 3,
+            role: 'hauler',
+            body: [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY],
+            type: 'swarm',
+            energy_room: 'W32S51'
+        },
+        {
+            count: 3,
+            role: 'hauler',
+            body: [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY],
+            type: 'swarm',
+            energy_room: 'W31S52'
+        },
+        {
+            count: 4,
+            role: 'hauler',
+            body: [MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY],
+            type: 'swarm',
+            energy_room: 'W32S52'
+        }
+    ],
+    'W32S51': [{
+            count: 1,
+            role: 'claimer',
+            body: [CLAIM, CLAIM, MOVE, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 2,
+            role: 'builder',
+            body: [WORK, MOVE, CARRY, MOVE, WORK, CARRY, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 2,
+            role: 'repairer',
+            body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
+            type: 'swarm'
+        },
+        {
+            count: 3,
+            role: 'miner',
+            body: [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE],
+            type: 'swarm'
+        }
+    ],
+    'W31S52': [{
+            count: 1,
+            role: 'claimer',
+            body: [CLAIM, CLAIM, MOVE, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 2,
+            role: 'builder',
+            body: [WORK, MOVE, CARRY, WORK, MOVE, CARRY, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 2,
+            role: 'repairer',
+            body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
+            type: 'swarm'
+        },
+        {
+            count: 3,
+            role: 'miner',
+            body: [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE],
+            type: 'swarm'
+        }
+    ],
+    'W32S52': [{
+            count: 1,
+            role: 'claimer',
+            body: [CLAIM, CLAIM, MOVE, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 2,
+            role: 'builder',
+            body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 3,
+            role: 'miner',
+            body: [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE],
+            type: 'swarm'
+        },
+        {
+            count: 2,
+            role: 'repairer',
+            body: [WORK, MOVE, CARRY, MOVE, CARRY, MOVE, WORK],
+            type: 'swarm'
+        }
+    ]
+};
