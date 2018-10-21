abdca995dcf2c0ebe27fcae5422fb5e937ab6031
Andrey Oleynikov <my_olein@mail.ru>
2018-10-21 12:37:44 +0300

    Add correct period support for jobs

--- build/jobs.executor.js
+++ build/jobs.executor.js
@@ -2,7 +2,9 @@ var jobs = [require('job.miners_count'), require('job.builders_count')];
 module.exports = {
   run: () => {
     jobs.forEach(job => {
-      job.execute();
+      if (Game.time % job.period == 0) {
+        job.execute();
+      }
     });
   }
 };
