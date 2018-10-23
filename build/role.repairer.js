605674fd4d0134c5ff6e29fe2fc78f9193cf7fb0
Andrey Oleynikov <my_olein@mail.ru>
2018-10-23 23:45:43 +0300

    Refactor repairer to find nearest non-fullhp structure

--- build/role.repairer.js
+++ build/role.repairer.js
@@ -2,40 +2,7 @@ var upgrader_role = require('role.upgrader');
 
 var energy_behavior = require('behavior.get_energy');
 
-var containers = require('structure.container');
-
-var roads = require('structure.road');
-
 var move = require('behavior.move');
-
-var find_repairable = function (room) {
-  var repairable = room.find(FIND_MY_STRUCTURES);
-  var conts = containers.get(room);
-
-  for (var i = 0; i < conts.length; ++i) {
-    repairable.push(conts[i]);
-  }
-
-  var rds = roads.get(room);
-
-  for (var i = 0; i < rds.length; ++i) {
-    repairable.push(rds[i]);
-  }
-
-  var max_hp_part = 1.0;
-  var repairable_id = undefined;
-
-  for (var i = 0; i < repairable.length; ++i) {
-    var value = 1.0 * repairable[i].hits / repairable[i].hitsMax;
-
-    if (value < max_hp_part) {
-      max_hp_part = value;
-      repairable_id = repairable[i].id;
-    }
-  }
-
-  return repairable_id;
-};
 
 var repair = function (creep, struct) {
   var repair_result = creep.repair(struct);
@@ -53,34 +20,16 @@ var repair = function (creep, struct) {
 };
 
 var repair_my_sructures = function (creep) {
-  if (creep.memory['repairable_id'] === undefined) {
-    creep.memory['repairable_id'] = find_repairable(creep.room);
-  }
-
-  if (creep.memory['repairable_id'] === undefined) {
-    return false;
-  }
-
-  var struct = Game.getObjectById(creep.memory['repairable_id']);
-  if (!struct) return;
-  repair(creep, struct);
+  var repairables = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+    filter: s => s.hist < s.hitsMax
+  });
+  if (repairables.length == 0) return false;
+  repair(creep, repairables[0]);
   return true;
 };
 
-var repair_walls = function (creep) {
-  var walls = creep.room.find(FIND_STRUCTURES, {
-    filter: s => s.structureType == STRUCTURE_WALL && s.hits < s.hitsMax
-  });
-
-  if (walls.length == 0) {
-    return false;
-  }
-
-  repair(creep, walls[0]);
-};
-
 var work = function (creep) {
-  var busy = repair_my_sructures(creep) || repair_walls(creep);
+  var busy = repair_my_sructures(creep);
 
   if (!busy) {
     upgrader_role.perform(creep);
