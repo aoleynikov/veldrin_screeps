ba42f3e5748ca07fc4393c5ffafce07c0f408185
Andrey Oleynikov <my_olein@mail.ru>
2018-10-24 09:22:30 +0300

    Add construction sites to move behavior

--- src/behaviors/behavior.move.js
+++ src/behaviors/behavior.move.js
@@ -1,9 +1,17 @@
 var impassable = (item) => {
+    var obstacle = undefined;
+
     if (item.type == 'structure') {
-        return item.structure.structureType == STRUCTURE_SPAWN ||
-            item.structure.structureType == STRUCTURE_EXTENSION ||
-            item.structure.structureType == STRUCTURE_TOWER ||
-            item.structure.structureType == STRUCTURE_STORAGE;
+        obstacle = item.structure.structureType;
+    } else {
+        obstacle = item.constructionSite.structureType;
+    }
+
+    if (obstacle !== undefined) {
+        return obstacle == STRUCTURE_SPAWN ||
+            obstacle == STRUCTURE_EXTENSION ||
+            obstacle == STRUCTURE_TOWER ||
+            obstacle == STRUCTURE_STORAGE;
     }
     return item.type == 'creep';
 }
