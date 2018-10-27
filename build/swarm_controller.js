e0222d24543cafdc2c15c52c2b1e68b6167fc102
Andrey Oleynikov <my_olein@mail.ru>
2018-10-27 22:32:28 +0300

    Refactor spawn controller to move spawns iteration to module's main function

--- build/swarm_controller.js
+++ build/swarm_controller.js
@@ -1,34 +1,26 @@
 var population = require('population');
 
 var controller = {
-  spawnCreep: function (template) {
+  spawnCreep: function (spawn, template) {
     var logging = Game.time % 50 == 0;
+    if (spawn.spawning) return;
 
-    for (var spawn_name in Game.spawns) {
-      var spawn = Game.spawns[spawn_name];
-      if (spawn.spawning) continue;
-      var spawn_done = false;
+    for (var i = 0; i < template.count; ++i) {
+      var name = template.name_prefix + i;
 
-      for (var i = 0; i < template.count; ++i) {
-        var name = template.name_prefix + i;
-
-        if (Game.creeps[name]) {
-          if (logging) console.log('Already exists:', name);
-          continue;
-        }
-
-        var spawnResult = spawn.spawnCreep(template.body, name, {
-          memory: template.memory
-        });
-
-        if (spawnResult == ERR_NOT_ENOUGH_ENERGY) {
-          if (logging) console.log('Not enough energy to spawn:', name);
-          spawn_done = true;
-          break;
-        }
+      if (Game.creeps[name]) {
+        if (logging) console.log('Already exists:', name);
+        continue;
       }
 
-      if (spawn_done) break;
+      var spawnResult = spawn.spawnCreep(template.body, name, {
+        memory: template.memory
+      });
+
+      if (spawnResult == ERR_NOT_ENOUGH_ENERGY) {
+        if (logging) console.log('Not enough energy to spawn:', name);
+        return;
+      }
     }
   } // swarm is a set of very cheap creeps doing certain tasks.
   // most of creep code will still be handeled by role model,
@@ -38,7 +30,10 @@ var controller = {
 module.exports = {
   respawn: function () {
     for (var template of Game.spawns['Main'].memory['population']['templates']) {
-      controller.spawnCreep(template);
+      for (var spawn_name in Game.spawns) {
+        var spawn = Game.spawns[spawn_name];
+        controller.spawnCreep(spawn, template);
+      }
     }
   }
 };
