16a135e218477ff41cfdc7a0a22a90e2b6f1b817
Andrey Oleynikov <my_olein@mail.ru>
2018-11-07 23:40:12 +0300

    Make energy-seeking creeps withdraw energy from tombstones in their proximity

--- src/behaviors/behavior.get_energy.js
+++ src/behaviors/behavior.get_energy.js
@@ -7,6 +7,20 @@ module.exports = {
 			this.refill(creep);
 		}
 
+		tombstones = creep.room.lookForAtArea(
+			LOOK_TOMBSTONES, 
+			creep.pos.y - 1, 
+			creep.pos.x - 1, 
+			creep.pos.y + 1,
+			creep.pos.x + 1, 
+			true)
+
+		for(var stone of tombstones) {
+			if(stone.store[RESOURCE_ENERGY] > 0) {
+				creep.withdraw(RESOURCE_ENERGY, stone.structure);
+			}
+		}
+
 		var room = undefined;
 		if (creep.memory['refill']) {
 			room = creep.memory['energy_room'] || creep.room.name;
