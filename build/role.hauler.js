3e81b68eaf629e9851418847caa7d52e11cd0eff
Andrey Oleynikov <my_olein@mail.ru>
2018-11-13 21:11:34 +0300

    Build previous changes

--- build/role.hauler.js
+++ build/role.hauler.js
@@ -1,5 +1,7 @@
 var energy_behavior = require('behavior.get_energy');
 
+var storages = require('structure.storage');
+
 var storeEnergy = function (creep, storage) {
   var store = creep.transfer(storage, RESOURCE_ENERGY);
 
@@ -14,11 +16,24 @@ module.exports = {
   perform: function (creep) {
     if (energy_behavior.perform(creep)) return;
 
-    storage = creep.pos.getClosestByRange(FIND_MY_STRUCTURE, {
-      filter: s => {
-        return s.structureType == STRUCTURE_STORAGE || s.structureType == STRUCTURE_LINK
+    if (creep.carry[RESOURCE_ENERGY] == 0) {
+      energy_behavior.refill(creep);
+      return;
+    }
+
+    var link_id = creep.memory['link_id'];
+
+    if (link_id) {
+      var link = Game.getObjectById(link_id);
+
+      if (link && link.energy < link.energyCapacity) {
+        storeEnergy(creep, link);
+        return;
       }
-    });
-    storeEnergy(creep, storage);
+    }
+
+    var storage = storages.get(creep.room);
+    if (storage.length == 0) return;
+    storeEnergy(creep, storage[0]);
   }
 };
