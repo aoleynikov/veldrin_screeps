edb9e813a669b3649cc85a19893fce0bec95b61a
Andrey Oleynikov <my_olein@mail.ru>
2018-11-14 11:11:50 +0300

    Put haulers back on track

--- src/roles/role.hauler.js
+++ src/roles/role.hauler.js
@@ -1,5 +1,14 @@
 var energy_behavior = require('behavior.get_energy');
 
+var storeEnergy = function (creep, storage) {
+    var store = creep.transfer(storage, RESOURCE_ENERGY);
+    if (store == ERR_NOT_IN_RANGE) {
+        creep.moveTo(storage);
+    } else if (store == 0 && creep.carry[RESOURCE_ENERGY] == 0) {
+        energy_behavior.refill(creep);
+    }
+};
+
 module.exports = {
     perform: function (creep) {
         if (energy_behavior.perform(creep)) return;
@@ -11,6 +20,9 @@ module.exports = {
         });
         if (creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(storage);
+        }
+        if (creep.carry[RESOURCE_ENERGY] == 0) {
+            energy_behavior.refill(creep);
         }
     }
 }
