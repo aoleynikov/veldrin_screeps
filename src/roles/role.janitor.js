065a456f031ed669ccba0345313bfb7c5624b4ae
Andrey Oleynikov <my_olein@mail.ru>
2018-11-14 12:44:03 +0300

    Add the janitor role itself

---
+++ src/roles/role.janitor.js
@@ -0,0 +1,25 @@
+var room_travel = require('behavior.room_travel');
+
+module.exports = {
+  perform: function (creep) {
+    if (room_travel.perofrm(creep)) return;
+    if (creep.carry[RESOURCE_ENERGY] == 0) {
+      var container = creep.pos.findClosestByRange(FIND_STRUCTURE, {
+        filter: (s) => {
+          if (s.structureType != STRUCTURE_CONTAINER) return false;
+          return s.store[RESOURCE_ENERGY] > 0 && _.sum(s.store) > s.store[RESOURCE_ENERGY]
+        }
+      });
+      if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(container);
+      }
+    } else {
+      var storage = creep.room.find(FIND_STRUCTURE, {
+        filter: (s) => s.structureType == STRUCTURE_STORAGE
+      });
+      if (creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(storage);
+      }
+    }
+  }
+}
