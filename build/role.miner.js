6305e3601675280714bed99000452ea091e908f9
Andrey Oleynikov <my_olein@mail.ru>
2018-11-14 11:56:53 +0300

    Fix several errors in miners code

--- build/role.miner.js
+++ build/role.miner.js
@@ -3,14 +3,15 @@ var room_travel = require('behavior.room_travel');
 module.exports = {
   perform: function (creep) {
     if (room_travel.perform(creep)) return;
-    container = creep.findClosestByRange(FIND_STRUCTURES, {
+    container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
       filter: s => {
-        var look = _.filter(s.pos.lookFor(LOOK_CREEPS), c => c.memory['role'] == creep.memory['role']);
+        var look = _.filter(s.pos.lookFor(LOOK_CREEPS), c => c.memory['role'] == creep.memory['role'] && c.id != creep.id);
 
         if (look) return false;
         return s.findInRange(creep.memory['find'], 1) != null;
       }
     });
+    if (!container) return;
 
     if (creep.pos.x != container.pos.x || creep.pos.y != container.pos.y) {
       creep.moveTo(container);
