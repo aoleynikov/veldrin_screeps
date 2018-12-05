b063fd7b7006db283cc17fa7a5a59bd75212c367
Andrey Oleynikov <my_olein@mail.ru>
2018-12-05 22:01:36 +0300

    Distribute bulders, remove importing upgraders, add snipers

--- build/population.colony.js
+++ build/population.colony.js
@@ -4,6 +4,17 @@ var miners_count = room_id => {
   return Game.rooms[room_id].find(FIND_SOURCES_ACTIVE).length;
 };
 
+var claimers_count = room_id => {
+  var room = Game.rooms[room_id];
+  if (!room) return 1;
+
+  if (room.controller.owner.name == 'Veldrin') {
+    return 0;
+  }
+
+  return 1;
+};
+
 module.exports = function (room_name, room_id) {
   var room_postfix = '_' + room_name + '_';
   return [{
@@ -27,6 +38,22 @@ module.exports = function (room_name, room_id) {
       type: 'swarm',
       work_place: room_id,
       energy_room: room_id
+    }
+  }, {
+    count: claimers_count(room_id),
+    name_prefix: 'claimer' + room_postfix,
+    body: [CLAIM, MOVE],
+    memory: {
+      target: room_id
+    }
+  }, {
+    count: 2,
+    name_prefix: 'builder_from' + room_postfix,
+    body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+    memory: {
+      role: 'builder',
+      refill: true,
+      type: 'swarm'
     }
   }];
 };
--- src/population/population.common.js
+++ src/population/population.common.js
@@ -10,47 +10,21 @@ module.exports = function() {
     }
   },
   {
-    count: 6,
-    name_prefix: 'upgrader_from_Moon_',
-    body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
-    memory: {
-      role: 'upgrader',
-      refill: true, 
-      type: 'swarm',
-      work_place: 'W37S11',
-      energy_room: 'W38S11'
-    }
-  },
-  {
-    count: 6,
-    name_prefix: 'upgrader_from_Mars_',
-    body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
-    memory: {
-      role: 'upgrader',
-      refill: true, 
-      type: 'swarm',
-      work_place: 'W37S11',
-      energy_room: 'W37S12'
-    }
-  },
-  {
-    count: 6,
-    name_prefix: 'upgrader_from_ISS_',
-    body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
-    memory: {
-      role: 'upgrader',
-      refill: true, 
-      type: 'swarm',
-      work_place: 'W37S11',
-      energy_room: 'W36S11'
-    }
-  },
-  {
     count: 1,
     name_prefix: 'scout_',
     body: [MOVE],
     memory: {
       role: 'scout'
+    }
+  },
+  {
+    count: 2,
+    name_prefix: 'sniper_',
+    body: [RANGED_ATTACK, RANGED_ATTACK, MOVE, MOVE],
+    memory: {
+      role: 'sniper',
+      type: 'swarm',
+      squad: 'war'
     }
   }]
 }
