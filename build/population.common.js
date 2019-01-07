8121d4bd44cdd73b9bc064964110b0d39404af05
Andrey Oleynikov <my_olein@mail.ru>
2019-01-08 02:38:26 +0300

    Make builders smaller to allow building them in a single spawn

--- build/population.common.js
+++ build/population.common.js
@@ -21,7 +21,7 @@ module.exports = function () {
   return [{
     count: 3,
     name_prefix: 'builder_',
-    body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+    body: [WORK, CARRY, CARRY, MOVE, MOVE],
     memory: {
       role: 'builder',
       refill: true,
