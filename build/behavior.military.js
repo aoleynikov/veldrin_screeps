fda059da43151aea4c4381881c7357a6d47839fd
Andrey Oleynikov <my_olein@mail.ru>
2019-01-19 11:16:13 +0300

    Fix structures condition, add dissolver role

--- build/behavior.military.js
+++ build/behavior.military.js
@@ -1,6 +1,6 @@
 var enemies_find = [FIND_HOSTILE_CREEPS, FIND_STRUCTURES, FIND_HOSTILE_SPAWNS, FIND_HOSTILE_CONSTRUCTION_SITES];
 
-var target_filter = t => !t.owner || t.owner.name != 'Veldrin' && (t.structureType != undefined || t.structureType != STRUCTURE_CONTROLLER && t.structureType != STRUCTURE_KEEPER_LAIR && t.structureType != STRUCTURE_WALL && t.structureType != STRUCTURE_ROAD && t.structureType != STRUCTURE_CONTAINER);
+var target_filter = t => !t.owner || t.owner.name != 'Veldrin' && t.structureType != undefined && t.structureType != STRUCTURE_CONTROLLER && t.structureType != STRUCTURE_KEEPER_LAIR && t.structureType != STRUCTURE_WALL && t.structureType != STRUCTURE_ROAD && t.structureType != STRUCTURE_CONTAINER;
 
 module.exports = {
   on_guard: function (creep) {
---
+++ build/role.dissolver.js
@@ -0,0 +1,27 @@
+var hauler = require('role.hauler');
+
+var room_travel = require('behavior.room_travel');
+
+module.exports = {
+  perform: function (creep) {
+    if (creep.memory['refill']) {
+      if (creep.room.name != creep.memory['resource_room']) {
+        creep.memory['target'] = creep.memory['energy_room'];
+      }
+
+      if (room_travel.perform(creep)) return;
+      var building = creep.room.findClosestByRange(FIND_STRUCTURE, {
+        filter: s => {
+          return s.structureType == STRUCTURE_WALL || s.owner.name != 'Veldrin';
+        }
+      });
+      var work = creep.dismantle(building);
+
+      if (work == ERR_NOT_IN_RANGE) {
+        creep.moveTo(building);
+      }
+    } else {
+      hauler.perform(creep);
+    }
+  }
+};
