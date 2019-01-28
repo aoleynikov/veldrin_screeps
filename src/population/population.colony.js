1af52e609d4a8985e74f10fd55edf5173ca42d80
Andrey Oleynikov <my_olein@mail.ru>
2019-01-28 17:18:34 +0300

    Add more MOVE to warriors

--- src/population/population.colony.js
+++ src/population/population.colony.js
@@ -33,7 +33,7 @@ module.exports = function(room_name, room_id, metropolia_name) {
     {
       count: warriors_count(room_id),
       name_prefix: 'warrior' + room_postfix,
-      body: [TOUGH, TOUGH, TOUGH, TOUGH, ATTACK, ATTACK, ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE],
+      body: [TOUGH, TOUGH, TOUGH, TOUGH, ATTACK, ATTACK, ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],
       memory: {
         type: 'swarm',
         role: 'warrior',
