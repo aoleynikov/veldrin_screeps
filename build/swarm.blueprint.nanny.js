970565b262d904b6e37e73b705ad20576dc86eae
Andrey Oleynikov <my_olein@mail.ru>
2020-01-05 13:38:32 +0300

    Fix nanny blueprint

--- build/swarm.blueprint.nanny.js
+++ build/swarm.blueprint.nanny.js
@@ -4,24 +4,27 @@ module.exports = {
   extension: [CARRY, MOVE],
   base: [WORK],
   name_prefix: (core, room) => {
-    return "nanny_" + room.name + "_" + this.size(core) + "_";
+    return "nanny_" + room + "_" + this.size(core) + "_";
   },
   size: core => {
-    return [1, 2, 3, 3, 3][core.controller.level];
+    let core_room = Game.rooms[core];
+    return [1, 2, 3, 3, 3][core_room.controller.level];
   },
   count: (core, room) => {
     if (core != room) return 0;
+    let core_room = Game.rooms[core];
     return [1, 3, 3, 4, 4, 5, 1, 1, 1][core.controller.level];
   },
   memory: (core, room) => {
     return {
       swarm: false,
       role: "nanny",
-      energy_room: room.name,
-      workplace: core.name
+      energy_room: room,
+      workplace: core
     };
   },
   body: core => {
-    return common.build_body(unit_blueprints[type].base, unit_blueprints[type].extension, from.energyCapacityAvailable, this.level(from));
+    let core_room = Game.rooms[core];
+    return common.build_body(unit_blueprints[type].base, unit_blueprints[type].extension, core_room.energyCapacityAvailable, this.size(core));
   }
 };
--- src/swarm/blueprint/swarm.blueprint.nanny.js
+++ src/swarm/blueprint/swarm.blueprint.nanny.js
@@ -4,28 +4,31 @@ module.exports = {
   extension: [CARRY, MOVE],
   base: [WORK],
   name_prefix: (core, room) => {
-    return "nanny_" + room.name + "_" + this.size(core) + "_"
+    return "nanny_" + room + "_" + this.size(core) + "_"
   },
   size: (core) => {
-    return [1, 2, 3, 3, 3][core.controller.level]
+    let core_room = Game.rooms[core]
+    return [1, 2, 3, 3, 3][core_room.controller.level]
   },
   count: (core, room) => 
   {
     if (core != room) return 0
+    let core_room = Game.rooms[core]
     return [1, 3, 3, 4, 4, 5, 1, 1, 1][core.controller.level]
   },
   memory: (core, room) => { 
     return {
       swarm: false,
       role: "nanny",
-      energy_room: room.name,
-      workplace: core.name
+      energy_room: room,
+      workplace: core
     }
   },
   body: (core) => {
+    let core_room = Game.rooms[core]
     return common.build_body(unit_blueprints[type].base, 
     unit_blueprints[type].extension, 
-    from.energyCapacityAvailable, 
-    this.level(from))
+    core_room.energyCapacityAvailable, 
+    this.size(core))
   }
 }
